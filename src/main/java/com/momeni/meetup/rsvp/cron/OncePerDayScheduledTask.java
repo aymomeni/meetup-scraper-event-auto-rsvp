2822f29f43e0fb753de38da1bc58a91eb87e7759
momeni <aymomeni@gmail.com>
2024-01-01 17:38:38 -0700

    test

--- src/main/java/com/momeni/meetup/rsvp/cron/OncePerDayScheduledTask.java
+++ src/main/java/com/momeni/meetup/rsvp/cron/OncePerDayScheduledTask.java
@@ -24,7 +24,8 @@ public class OncePerDayScheduledTask {
 
     private static final Logger log = LoggerFactory.getLogger(OncePerDayScheduledTask.class);
 
-    @Scheduled(cron = "0 0 14 * * *")
+    // 0 0 14 * * * utc (every day at 7am)
+    @Scheduled(cron = "0 */2 * * * *")
     public void dailyEventTaskScheduler() throws IOException {
         log.info("Main Scheduler started.. ");
         List<String> eventUrls = meetupApiService.getAllEventUrls();
