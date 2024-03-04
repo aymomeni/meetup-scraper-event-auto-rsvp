bfe4cd280a4144745f49efbc4478238958a5ffc0
momeni <aymomeni@gmail.com>
2024-03-03 19:26:35 -0700

    test

--- src/main/java/com/momeni/meetup/rsvp/service/MeetupScraperService.java
+++ src/main/java/com/momeni/meetup/rsvp/service/MeetupScraperService.java
@@ -57,7 +57,7 @@ public class MeetupScraperService {
             Date eventRsvpOpenDate = this.stringToISO8601(rsvpOpenDateStr);
 
             // if saturday indoor game rsvp to 2nd session (because I want to sleep in)
-            if(DateUtils.isSameDay(new Date(), eventRsvpOpenDate) && eventTitle.contains("East Millcreek") && eventTitle.contains("Sat") && eventTitle.contains("2nd")) {
+            if(DateUtils.isSameDay(new Date(), eventRsvpOpenDate) && eventTitle.contains("East Millcreek") && eventTitle.contains("Sat") && eventTitle.contains("1st")) {
                 Event event = new Event(eventTitle, eventUrl, eventRsvpOpenDate);
                 eventList.add(event);
             // else if tuesday indoor game rsvp to 1st session (because I've work the next day)
